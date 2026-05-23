b671bbbcd3dfe8926e80d6fc7f1cfa9c2572ad50
Sriranjani28 <sriranjanimanimaran728@gmail.com>
2026-05-23 11:55:27 +0530

    updated

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ io.emit('chat message', messageWithTime)
   })
 })
 const PORT = process.env.PORT || 3000
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
 })
